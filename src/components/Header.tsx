39ef99b788719ba906e5456497a4f5fd901911bf
Hadrien <smethadrien.96@gmail.com>
2023-02-24 19:29:08 +0100

    Had to add some padding top on each page since the header is in fixed position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import HamburgerBtn from "./mui/HamburgerBtn";
 
 const Header = () => {
     return (
         <header>
-            <span>Tinyclip</span>
+            <Link to="/">
+                <span>Tinyclip</span>
+            </Link>
             <HamburgerBtn />
         </header>
     );
