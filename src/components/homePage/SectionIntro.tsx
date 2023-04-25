2ee06ae58b021f1b8e34d413f9df0f6fab19b604
Hadrien <smethadrien.96@gmail.com>
2023-04-25 12:46:17 +0200

    Add some basic animations on the home page

--- src/components/homePage/SectionIntro.tsx
+++ src/components/homePage/SectionIntro.tsx
@@ -1,7 +1,16 @@
 const SectionIntro = () => {
     return (
         <section className="intro">
-            <h1>Tinyclip</h1>
+            <h1>
+                <span>T</span>
+                <span>i</span>
+                <span>n</span>
+                <span>y</span>
+                <span>c</span>
+                <span>l</span>
+                <span>i</span>
+                <span>p</span>
+            </h1>
             <div className="bg-circle"></div>
         </section>
     );
